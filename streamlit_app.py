6d00645f2ecdb431e1f3aad4ce6c227a0e7f350f
bribrixcolibrix <75209546+bribrixcolibrix@users.noreply.github.com>
2025-04-18 17:52:30 +0200

    add name to recipe

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,9 @@ st.write(
   """
 )
 
+name_on_order = st.text_input('Name on Smoothie')
+st.write('The name on your Smoothie will be :', name_on_order)
+
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
@@ -39,14 +42,15 @@ if ingredients_list:
 
     # st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     # st.write(my_insert_stmt)
+    
     time_to_insert = st.button('Submit order')
     if time_to_insert: 
         session.sql(my_insert_stmt).collect()
 
-        st.success('Your Smoothie is ordered!', icon="✅")
+        st.success(f'Your Smoothie is ordered, {name_on_order} !', icon="✅")
 
 
